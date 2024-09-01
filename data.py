could not fix backprop error

data.py
@@ -5,36 +5,44 @@ import random
 
 
 class UniversalEmbeddings(Dataset):
-    def __init__(self, kwargs):
-        self.kwargs = kwargs
-        self.image_embeddings = {} # to be loaded from file later
-        self.text_embeddings = {}  # to be loaded from file later
-        self.image_encoder_data = {} # to be created later
+    def __init__(self, image_data_path, text_data_path):
+        self.image_embeddings = torch.load(image_data_path)
+        self.image_encoder_data = {k: list(v.keys()) for k, v in self.image_embeddings.items()}
+        self.image_encoders = [name for v in self.image_encoder_data.values() for name in v]
+        self.num_image_encoders = sum([len(v) for v in self.image_encoder_data.values()])
 
-        self.num_image_encoders = len(list(self.image_embeddings.keys()))
-        self.num_text_encoders = len(list(self.text_embeddings.keys()))
-        self.text_embed_dim = kwargs["text_embed_dim"]
+        self.text_embeddings = torch.load(text_data_path)
+        self.text_embed_dim = list(self.text_embeddings.keys())[0]
+        self.text_encoder = list(self.text_embeddings[self.text_embed_dim].keys())[0]
+        self.num_text_encoders = 1
+
+        self.num_samples = self.text_embeddings[self.text_embed_dim][self.text_encoder].shape[0]
+
 
     def __len__(self):
-        return len(self.image_embeddings)
+        return self.num_samples
 
     def get_minibatch(self, batch_indices, sampled_encoder_indices, encoder_dims):
         # first get the encoder names from indices
-        sampled_encoders = [list(self.image_embeddings.keys())[i] for i in sampled_encoder_indices]
+        sampled_encoders = [self.image_encoders[i] for i in sampled_encoder_indices]
+        print(sampled_encoders, encoder_dims[0])
+
         # remember to not shuffle the data
         start, end = batch_indices[0], batch_indices[-1]+1
 
         # now get the image embeddings
+        chosen_dim = encoder_dims[0]
         image_embeddings = torch.cat(
-            [self.image_embeddings[k][start : end, :].unsqueeze(1) for k in sampled_encoders],
+            [self.image_embeddings[chosen_dim][k][start : end, :].unsqueeze(1) for k in sampled_encoders],
             dim=1
         )
-        assert image_embeddings.shape == [end-start, len(sampled_encoders), encoder_dims[0]], "Image embeddings are incorrectly stacked!"
+        assert image_embeddings.shape == torch.Size([end-start, len(sampled_encoders), chosen_dim]), "Image embeddings are incorrectly stacked!"
 
         # get text embeddings
-        text_embeddings = self.text_embeddings[self.text_encoder][start : end, :]
-        assert text_embeddings.shape == [end-start, self.text_embed_dim], "Text embeddings are incorrectly stacked!"
+        text_embeddings = self.text_embeddings[self.text_embed_dim][self.text_encoder][start : end, :]
+        assert text_embeddings.shape == torch.Size([end-start, self.text_embed_dim]), "Text embeddings are incorrectly stacked!"
 
+        # return the data
         return image_embeddings, text_embeddings
 
 
@@ -50,10 +58,6 @@ def init_indices_loader(args, dataset):
 
 
 def init_encoder_loader(args, dataset):
-    """
-    NOTE: Since we have image encoders of varying embedding dims,
-    => args.encoder_batch_size <= min([len(v) for v in dataset.image_encoder_data.values()]).
-    """
     num_encoders = sum([len(v) for v in dataset.image_encoder_data.values()])
     batch_size = args.encoder_batch_size
 

train.py
@@ -1,14 +1,28 @@
+# libraries
+import math
+from model import UniversalHyperNetwork
 import torch
 from torch.utils.data import DataLoader
 from hypnettorch.mnets import MLP
 from hypnettorch.hnets import HMLP
 import numpy as np
+import argparse
+from contextlib import suppress
+import warnings
+from tqdm import tqdm
 
+# local code imports
 from data import *
+from utils import *
+from model import *
+
+# shut off warnings
+warnings.simplefilter("ignore")
+# switch on anomaly detection
+# torch.autograd.set_detect_anomaly(True)
 
 
-def train_one_epoch(args, models, data_objects, criterion, optimizer, scheduler, scaler, epoch):
-    (hnet, main_net) = models
+def train_one_epoch(args, hnet, data_objects, criterion, optimizer, scheduler, scaler, epoch):
     hnet.train()
     (dataset, indices_loader, encoder_loader) = data_objects
 
@@ -18,15 +32,19 @@ def train_one_epoch(args, models, data_objects, criterion, optimizer, scheduler,
     total = 0
     running_loss = 0
     total_loss = 0
-    autocast = torch.cuda.amp.autocast
+    autocast = suppress # NOTE: edit later
     logit_scale = torch.tensor(np.log(100.0)).to(args.device)
 
+    num_steps = math.ceil(len(dataset) / args.batch_size)
+    bar = tqdm(total=num_steps)
     for idx, batch_indices in enumerate(indices_loader):
         # first get the encoders for which we are observing the data stream
         encoder_indices, encoder_dims = next(encoder_loader)
         # get the data stream
         image_features, text_features = dataset.get_minibatch(batch_indices, encoder_indices, encoder_dims)
-        [B, N, D] = image_features.shape
+        [B, N, D_img] = image_features.shape
+        # image_features = pad_image_features(image_features, args.largest_image_dim - D_img)
+        D_txt = text_features.shape[-1]
 
         # cast embeddings
         image_features = image_features.float().to(args.device)
@@ -39,13 +57,25 @@ def train_one_epoch(args, models, data_objects, criterion, optimizer, scheduler,
 
         optimizer.zero_grad()
 
+        outputs = None
+
         # forward pass
+        params = []
         with autocast():
-            params = hnet(cond_id=encoder_indices)
+            print(encoder_indices)
+            # `mapped_text_features` has length = num_total_image_encoders
+            outputs = hnet(
+                conditions=encoder_indices,
+                input_features=text_features,
+                slice_dims=(D_img, D_txt)
+            )
+
             for i in range(N):
-                mapped_text_features = main_net(text_features, weights=params[i])
+                # now get the loss and predictions
+                # mapped_text_features = text_features @ outputs[i][0][:D_img, :D_txt].T + outputs[i][1][:D_img]
+
                 loss, inbatch_corrects = criterion.compute_loss_and_accuracy(
-                    logit_scale, image_features[:, i, :].view(B, D), mapped_text_features
+                    logit_scale, image_features[:, i, :].view(B, D_img), outputs[i]
                 )
 
                 total_loss += loss
@@ -61,20 +91,75 @@ def train_one_epoch(args, models, data_objects, criterion, optimizer, scheduler,
         running_loss /= N
 
         # backward pass
-        scaler.scale(total_loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
+        if scaler is not None:
+            scaler.scale(total_loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+        else:
+            total_loss.backward(retain_graph=args.retain_graph)
+            optimizer.step()
 
-    return {"avg_loss": running_loss / len(indices_loader), "accuracies": accuracies}
+        bar.update(1)
+
+    return {"avg_loss": running_loss / num_steps, "accuracies": accuracies}
 
 
 def main(args):
-    kwargs = {}
-    dataset = UniversalEmbeddings(kwargs)
+    # load in training data
+    kwargs = {
+        "image_data_path": "datasets/random_image_embeddings.pt",
+        "text_data_path": "datasets/random_text_embeddings.pt"
+    }
+    dataset = UniversalEmbeddings(**kwargs)
     index_loader = init_indices_loader(args, dataset)
     encoder_loader = init_encoder_loader(args, dataset)
 
-    main_net = MLP(n_in=args.largest_text_dim, hidden_layers=[], n_out=args.largest_image_dim).to(args.device)
-    hnet = HMLP(main_net.param_shapes, uncond_emb_size=0, cond_emb_size=args.image_embed_dim, num_cond_embs=args.num_image_encoders)
-    hnet.apply_hyperfan_init(mnet=main_net)
-    hnet = hnet.to(args.device)
+    # load in the hyper-network
+    args.num_image_encoders = dataset.num_image_encoders
+    model = Custom2(args)
+
+    # optimizer and learning rate scheduler
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    scheduler = None
+
+    # loss function and grad-scaler
+    criterion = ClipLoss()
+    scaler = None
+
+    # training loop
+    for epoch in range(args.num_epochs):
+        train_logs = train_one_epoch(
+            args, model, (dataset, index_loader, encoder_loader),
+            criterion, optimizer, scheduler, scaler, epoch
+        )
+        print(train_logs)
+        break
+
+
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+    # overall settings
+    parser.add_argument("--device", type=str, default="cpu")
+    # model settings
+    parser.add_argument("--largest-image-dim", type=int, default=1024)
+    parser.add_argument("--largest-text-dim", type=int, default=768)
+    parser.add_argument("--num-image-encoders", type=int, default=1)
+    parser.add_argument("--encoder-batch-size", type=int, default=4)
+    parser.add_argument("--hnet-cond-emb-dim", type=int, default=64)
+    parser.add_argument("--return-params", type=bool, default=False)
+    # training settings
+    parser.add_argument("--num-epochs", type=int, default=1)
+    parser.add_argument("--batch-size", type=int, default=20)
+    parser.add_argument("--learning-rate", type=float, default=1e-3)
+    parser.add_argument("--weight-decay", type=float, default=0.0)
+    parser.add_argument("--retain-graph", type=bool, default=False)
+
+    # return args
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = setup_args()
+    main(args)